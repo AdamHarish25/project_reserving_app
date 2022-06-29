0515c00f259884ef52014f3673b539cf5ecd38ef
adam <adamharits25@gmail.com>
2022-06-29 08:22:07 +0700

    Homepage progress

--- lib/Icons.dart
+++ lib/Icons.dart
@@ -1,6 +1,9 @@
+// ignore_for_file: constant_identifier_names, file_names
+
 import 'package:flutter/material.dart';
 
-/// <i class="material-icons-round md-36">card-text</i> &#x2014; material icon named "Card".
+/// <i class="material-icons-round md-36">card</i> &#x2014; material icon named "Card".
 const IconData CardText = IconData(0xe934, fontFamily: 'Card');
 
+///
 const IconData Heart = IconData(0xe954, fontFamily: 'Heart');
